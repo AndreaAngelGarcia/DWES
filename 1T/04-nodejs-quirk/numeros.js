5210942b2507aa5dbe74848cbfc4f139fa1d7628
Andrea Ángel García <andrea.angel@adaits.es>
2022-10-11 11:26:49 +0200

    Ejercicio clase numeros

--- 1T/04-nodejs-quirk/numeros.js
+++ 1T/04-nodejs-quirk/numeros.js
@@ -1,9 +1,10 @@
+/* Eliminar todos los elementos inferiores a 18 de un array de números */
+
 const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+const resultado = numeros.filter(numerosMenor);
 
-const result = numeros.filter(numerosMenor);
+  function numerosMenor(numeros){
+    return numeros >= 18;
+  }
 
-function numerosMenor(numeros){
-  return numeros > 18;
-}
-
-console.log(result);
+console.log(resultado);

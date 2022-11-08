aa0b7eda8f9f87e99fe5be4a7546879182ee2754
Andrea Ángel García <andrea.angel@adaits.es>
2022-11-08 15:39:07 +0100

    Ejercicio 2 casa
    Ejercicio comparar fechas

--- 1T/11-unit-test/src/dateCompare.js
+++ 1T/11-unit-test/src/dateCompare.js
@@ -1,2 +1,35 @@
+import {DateTime} from 'luxon';
 
+function dateCompare(fecha1, fecha2) {
+    if (fecha1 < fecha2) {
+        const fecha = {
+            startDate: fecha1, 
+            endDate: fecha2
+        }
+        return fecha;
+    } else if (fecha1 > fecha2) {
+        const fecha = {
+            startDate: fecha2, 
+            endDate: fecha1
+        }
+        return fecha;
+    } else {
 
+        fecha2 = DateTime.now().toFormat("dd/MM/yyyy");
+
+        if (fecha1 < fecha2) {
+            const fecha = {
+                startDate: fecha1, 
+                endDate: fecha2
+            }
+            return fecha;
+        } else if (fecha1 > fecha2) {
+            const fecha = {
+                startDate: fecha2, 
+                endDate: fecha1
+            }
+            return fecha;
+        } 
+    }
+}
+export default dateCompare;

59c45534ca802fbc8f6ae308429ccfb111c4fb7b
Andrea Ángel García <andrea.angel@adaits.es>
2022-11-08 23:34:31 +0100

    Ejercicio 1 fizzbuzz
    Actualización test

--- 1T/11-unit-test/test/fizzbuzz.test.js
+++ 1T/11-unit-test/test/fizzbuzz.test.js
@@ -3,12 +3,12 @@ import fizzbuzz from "../src/fizzbuzz.js";
 
 test("Fizzbuzz = 1", t =>{
     const result = fizzbuzz(1);
-    t.is(result,1);
+    t.is(result, 1);
 });
 
 test("Fizzbuzz = 2", t =>{
     const result = fizzbuzz(2);
-    t.is(result,2);
+    t.is(result, 2);
 });
 
 test("Fizzbuzz = 3", t =>{
@@ -18,8 +18,8 @@ test("Fizzbuzz = 3", t =>{
 
 test("Fizzbuzz = 4", t =>{
     const result = fizzbuzz(4);
-    t.is(result,4);
-})
+    t.is(result, 4);
+});
 
 test("Fizzbuzz = 5", t =>{
     const result = fizzbuzz(5);

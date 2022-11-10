9bc5cb006b4f4b7413bbaa702656776e0edfedc5
Andrea Ángel García <andrea.angel@adaits.es>
2022-11-10 19:51:05 +0100

    Ejercicios clase

--- 1T/12-setup-workspace/ejercicio3-4.js
+++ 1T/12-setup-workspace/ejercicio3-4.js
@@ -1,7 +1,6 @@
 import { blue } from 'chalk';
 
 const youShouldNeverUseVar = 'This is my very long line that eslint should check as an error ............................................';
-// He imprimido abajo youShouldNeverUseVar
 
 const nonExistingVar = 'no existo';
 // He creado nonExistingVar

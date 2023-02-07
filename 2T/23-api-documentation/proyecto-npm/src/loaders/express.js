2321d1f3cb3e01812c81ebd26b71eb62e5a3d59c
Andrea Ángel García <andrea.angel@adaits.es>
2023-02-07 11:34:49 +0100

    Actualización final
    Ejercicio hecho completo

--- 2T/23-api-documentation/proyecto-npm/src/loaders/express.js
+++ 2T/23-api-documentation/proyecto-npm/src/loaders/express.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
 const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('../openai/swagger.json');
+// const OpenApiValidator = require('express-openapi-validator');
+const swaggerDocument = require('../openapi');
 const routes = require('../routes');
 
 module.exports = server => {
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
+  /* server.use(
+    OpenApiValidator.middleware({
+      apiSpec: swaggerDocument,
+      validateRequests: true, // (default)
+      validateResponses: true, // false by default
+    }),
+  ); */
   server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
   server.use(routes);
   server.use((req, res) => res.status(404).send({ message: 'Not Found' }));
